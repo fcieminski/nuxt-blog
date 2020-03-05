636f3dc5f84d4d70d5a6ccb9e6d8e8d33721a268
Filip Chamier-Ciemiński <45480039+fcieminski@users.noreply.github.com>
2020-03-05 21:49:42 +0100

    font added, header in progress

---
+++ components/TopBar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <v-app-bar color="#00695C">
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            <v-toolbar-title>Page title</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+                <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+        </v-app-bar>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TopBar',
+    data() {
+        return {}
+    },
+    components: {},
+    created() {},
+    computed: {},
+    methods: {}
+}
+</script>
+
+<style lang='sass' scoped>
+
+</style>
