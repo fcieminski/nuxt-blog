e7a0074173374fb8d3b8a142015962800f51a950
Filip Chamier-Ciemiński <45480039+fcieminski@users.noreply.github.com>
2020-03-07 16:11:41 +0100

    added footer

---
+++ components/BlogFooter.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-footer dark padless>
+        <v-card flat tile class="footer--color white--text text-center">
+            <v-card-text>
+                <v-btn
+                    v-for="icon in icons"
+                    :key="icon"
+                    class="mx-4 white--text"
+                    icon
+                >
+                    <v-icon size="24px">{{ icon }}</v-icon>
+                </v-btn>
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-text class="white--text text--align">
+                {{ new Date().getFullYear() }} —
+                <strong>Filip Chamier-Ciemiński</strong>
+            </v-card-text>
+        </v-card>
+    </v-footer>
+</template>
+
+<script>
+export default {
+    name: 'BlogFooter',
+    data() {
+        return {
+            icons: ['mdi-facebook', 'mdi-linkedin', 'mdi-github-circle']
+        }
+    }
+}
+</script>
+
+<style lang='scss' scoped>
+.footer--color {
+    background-color: #004d40;
+    width: 100%;
+    text-align: center;
+    .text--align {
+        text-align: initial;
+    }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -6,41 +6,20 @@
                 <nuxt />
             </div>
         </main>
-        <v-footer dark padless fixed>
-            <v-card flat tile class="footer--color white--text text-center">
-                <v-card-text>
-                    <v-btn
-                        v-for="icon in icons"
-                        :key="icon"
-                        class="mx-4 white--text"
-                        icon
-                    >
-                        <v-icon size="24px">{{ icon }}</v-icon>
-                    </v-btn>
-                </v-card-text>
-                <v-divider></v-divider>
-                <v-card-text class="white--text">
-                    {{ new Date().getFullYear() }} — <strong>Filip Chamier-Ciemiński</strong>
-                </v-card-text>
-            </v-card>
-        </v-footer>
+        <blog-footer />
     </div>
 </template>
 
 <script>
 import TopBar from '../components/TopBar.vue'
+import BlogFooter from '../components/BlogFooter.vue'
 export default {
     components: {
-        TopBar
+        TopBar,
+        BlogFooter
     },
     data() {
-        return {
-            icons: [
-                'mdi-facebook',
-                'mdi-linkedin',
-                'mdi-github-circle'
-            ]
-        }
+        return {}
     }
 }
 </script>
@@ -48,14 +27,10 @@ export default {
 <style lang="scss" scoped>
 .main {
     background-color: #cfd8dc;
-    height: calc(100vh - 64px);
     .main__container {
-        
+        margin: 0 auto;
+        width: 1200px;
+        height: calc(100vh - 64px);
     }
-}
-.footer--color{
-    background-color: #004D40;
-    width: 100%;
-    text-align: center;
 }
 </style>
