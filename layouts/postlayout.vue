c689ef17bfdbb3198e30dd5c2bf35ccf16f3913c
Filip Chamier-Ciemiński <f.cieminski@gmail.com>
2020-03-26 19:47:22 +0100

    styling post page almost done

--- layouts/postlayout.vue
+++ layouts/postlayout.vue
@@ -1,116 +1,108 @@
 <template>
-    <v-app>
-        <top-bar />
-        <div class="main__box">
-            <main class="main">
-                <transition tag="div" class="main__container" name="fade">
-                    <nuxt />
-                </transition>
-            </main>
-        </div>
+	<v-app>
+		<top-bar />
+		<div class="main__box">
+			<main class="main">
+				<transition tag="div" class="main__container" name="fade">
+					<nuxt :current-scroll-position="currentScrollPosition" />
+				</transition>
+			</main>
+		</div>
 
-        <div
-            :class="{ 'drawer--show': iconDrawer }"
-            class="button__box"
-            ref="drawer"
-        >
-            <div class="d-flex flex-column">
-                <v-btn v-for="icon in icons" :key="icon" icon>
-                    <v-icon>{{ icon }}</v-icon>
-                </v-btn>
-            </div>
-            <v-btn @click="iconDrawer = !iconDrawer" icon>
-                <v-icon>mdi-chevron-right</v-icon>
-            </v-btn>
-        </div>
-        <v-fab-transition>
-            <v-btn
-                v-if="scrolled"
-                color="#e68139"
-                fab
-                dark
-                small
-                fixed
-                bottom
-                right
-                @click="goUp"
-            >
-                <v-icon>mdi-chevron-up</v-icon>
-            </v-btn>
-        </v-fab-transition>
-    </v-app>
+		<div :class="{ 'drawer--show': iconDrawer }" class="button__box" ref="drawer">
+			<div class="d-flex flex-column">
+				<v-btn v-for="icon in icons" :key="icon" icon>
+					<v-icon>{{ icon }}</v-icon>
+				</v-btn>
+			</div>
+			<v-btn @click="iconDrawer = !iconDrawer" icon>
+				<v-icon>mdi-chevron-right</v-icon>
+			</v-btn>
+		</div>
+		<v-fab-transition>
+			<v-btn v-if="scrolled" color="#e68139" fab dark small fixed bottom right @click="goUp">
+				<v-icon>mdi-chevron-up</v-icon>
+			</v-btn>
+		</v-fab-transition>
+	</v-app>
 </template>
 
 <script>
-import TopBar from '../components/TopBar.vue'
-import BlogFooter from '../components/BlogFooter.vue'
-import BlogAside from '../components/BlogAside.vue'
-export default {
-    components: {
-        TopBar,
-        BlogFooter,
-        BlogAside
-    },
+	import TopBar from '../components/TopBar.vue'
+	import BlogFooter from '../components/BlogFooter.vue'
+	import BlogAside from '../components/BlogAside.vue'
+	export default {
+		components: {
+			TopBar,
+			BlogFooter,
+			BlogAside
+		},
 
-    data() {
-        return {
-            scrolled: false,
-            html: null,
-            icons: ['mdi-facebook', 'mdi-linkedin', 'mdi-github'],
-            iconDrawer: false
-        }
-    },
+		data() {
+			return {
+				scrolled: false,
+				html: null,
+				icons: ['mdi-facebook', 'mdi-linkedin', 'mdi-github'],
+				iconDrawer: false,
+				currentScrollPosition: 0
+			}
+		},
 
-    mounted() {
-        this.html = document.querySelector('html')
-        window.addEventListener('scroll', this.scrollPosition)
-    },
+		mounted() {
+			this.html = document.querySelector('html')
+			window.addEventListener('scroll', this.scrollPosition)
+		},
 
-    methods: {
-        scrollPosition() {
-            if (this.html.scrollTop >= this.html.scrollTopMax / 2) {
-                this.scrolled = true
-            } else {
-                this.scrolled = false
-            }
-        },
-        goUp() {
-            this.html.scrollTo({ top: 0, behavior: 'smooth' })
-        }
-    }
-}
+		methods: {
+			scrollPosition() {
+				if (this.html.scrollTop >= this.html.scrollTopMax / 2) {
+					this.scrolled = true
+				} else {
+					this.scrolled = false
+				}
+				this.currentScrollPosition = window.scrollY
+			},
+			goUp() {
+				this.html.scrollTo({ top: 0, behavior: 'smooth' })
+			}
+		},
+
+		destroyed() {
+			window.removeEventListener('scroll', this.scrollPosition)
+		}
+	}
 </script>
 
 <style lang="scss" scoped>
-.main__box {
-    background-color: white;
-    width: 100vw;
-    height: 100vh;
-    .main {
-        display: flex;
-        justify-content: space-between;
-        padding-left: 20px;
-        padding-right: 20px;
-        margin: 56px auto;
-        width: 90%;
-        .main__container {
-            width: 60vw;
-        }
-    }
-}
-.button__box {
-    position: fixed;
-    padding: 8px 0;
-    display: flex;
-    background-color: $secondary-light-accent;
-    align-items: center;
-    top: 55vh;
-    left: -40px;
-    border-top-right-radius: 4px;
-    border-bottom-right-radius: 4px;
-    transition: left 0.4s;
-    &.drawer--show {
-        left: 0px;
-    }
-}
+	.main__box {
+		background-color: white;
+		width: 100vw;
+		height: 100vh;
+		.main {
+			display: flex;
+			justify-content: space-between;
+			padding-left: 20px;
+			padding-right: 20px;
+			margin: 56px auto;
+			width: 90%;
+			.main__container {
+				width: 60vw;
+			}
+		}
+	}
+	.button__box {
+		position: fixed;
+		padding: 8px 0;
+		display: flex;
+		background-color: $secondary-light-accent;
+		align-items: center;
+		top: 55vh;
+		left: -40px;
+		border-top-right-radius: 4px;
+		border-bottom-right-radius: 4px;
+		transition: left 0.4s;
+		&.drawer--show {
+			left: 0px;
+		}
+	}
 </style>
--- pages/_post.vue
+++ pages/_post.vue
@@ -1,8 +1,22 @@
 <template>
-	<div class="post__container">
+	<div v-if="post" class="post__container">
 		<transition class="post" name="fade" tag="div">
 			<component :is="postComponent" />
 		</transition>
+
+		<v-list light class="content__box">
+			<v-list-item
+				:input-value="currentActive"
+				ripple
+				class="cp"
+				v-for="(content, key, index) in post.tableOfContent"
+				:key="index"
+			>
+				<v-list-item-content @click="goToElement(key)">
+					<v-list-item-title>{{ content }}</v-list-item-title>
+				</v-list-item-content>
+			</v-list-item>
+		</v-list>
 	</div>
 </template>
 
@@ -19,31 +33,48 @@
 			} catch (err) {
 				console.debug(err)
 			}
-		},
+        },
+        
+        props: {
+            currentScrollPosition: {
+                type: Number,
+                default: 0
+            }
+        },
 
 		data() {
 			return {
 				post: null,
-				postComponent: null
+				postComponent: null,
+				currentActive: null
+			}
+		},
+
+		methods: {
+			goToElement(key) {
+				const element = document.querySelector(key)
+				const offset = 20
+				const bodyRect = document.body.getBoundingClientRect().top
+				const elementRect = element.getBoundingClientRect().top
+
+				const possitionToScroll = elementRect - bodyRect - offset
+
+				window.scrollTo({ top: possitionToScroll, behavior: 'smooth' })
 			}
 		}
 	}
 </script>
 
-<style lang='scss' scoped>
-			blockquote {
-				padding: 16px 0px;
-				margin: 16px 0px;
-				border: 1px solid black;
-				p {
-					padding: 16px;
-					margin: 0;
-				}
-			}
+<style lang="scss" scoped>
 	.post__container {
 		margin: 18px auto;
 		width: 65%;
-		.frontmatter-markdown {
-		}
+	}
+
+	.content__box {
+		top: 70px;
+		position: fixed;
+		left: 10px;
+		border-radius: 4px;
 	}
 </style>
